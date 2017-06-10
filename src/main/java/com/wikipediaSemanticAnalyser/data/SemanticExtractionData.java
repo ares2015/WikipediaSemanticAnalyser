495414f1fb9f2cbb8f90c4f88aee725190f27b98
Oliver Eder <eder@essential-data.sk>
2017-06-10 22:53:50 +0200

    Initial commit

---
+++ src/main/java/com/wikipediaSemanticAnalyser/data/SemanticExtractionData.java
@@ -0,0 +1,7 @@
+package com.wikipediaSemanticAnalyser.data;
+
+/**
+ * Created by Oliver on 6/10/2017.
+ */
+public class SemanticExtractionData {
+}
---
+++ src/main/java/com/wikipediaSemanticAnalyser/database/DatabaseInserter.java
@@ -0,0 +1,9 @@
+package com.wikipediaSemanticAnalyser.database;
+
+/**
+ * Created by Oliver on 6/10/2017.
+ */
+public interface DatabaseInserter {
+
+
+}
